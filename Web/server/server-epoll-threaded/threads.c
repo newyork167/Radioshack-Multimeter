cac0a387085f5e002e1b7258e1b2d18f92bc8f40
James Rogers <jamesmrogers@gmail.com>
2024-08-19 05:35:08 -0400

    added thread safety

--- Web/server/server-epoll-threaded/threads.c
+++ Web/server/server-epoll-threaded/threads.c
@@ -2,16 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <fcntl.h>
 #include <sys/sendfile.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
 #include <sched.h>
 #include <pthread.h>
-
 
 typedef struct thread
 {
@@ -23,25 +20,23 @@ typedef struct thread
   struct thread *prev;
 } thread;
 
-
 struct thread *freelist;
 struct thread *worklist;
-
+pthread_mutex_t freelist_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t worklist_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 /* Parse and return file handle for http get request */
-int
-OpenFile (char *buf, int n)
+int OpenFile (char *buf, int n)
 {
-
   int i, j;
-  char *index = { "index.html" };
+  char *index = "index.html";
   char path[1024];
 
   if (buf == NULL || n < 10)
     return 0;
 
   // Ensure that the string starts with "GET "
-  if (!buf[0] == 'G' || !buf[1] == 'E' || !buf[2] == 'T' || !buf[3] == ' ')
+  if (!(buf[0] == 'G' && buf[1] == 'E' && buf[2] == 'T' && buf[3] == ' '))
     return 0;
 
   // copy the string
@@ -63,17 +58,14 @@ OpenFile (char *buf, int n)
   printf ("Found path: \"%s\"\n", path);
 
   // the +1 removes the leading slash
-  return (open (path + 1, O_RDONLY));
+  return open (path + 1, O_RDONLY);
 }
 
-void
-HandleResonse (int fd, char *buf, int n)
+void HandleResonse (int fd, char *buf, int n)
 {
-  char badresponse[1024] =
-    {
-"HTTP/1.1 404 Not Found\r\n\r\n<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\r\n<TITLE>Not Found</TITLE></HEAD><BODY>\r\n<H1>Not Found</H1>\r\n</BODY></HTML>\r\n\r\n"
-};
-  char goodresponse[1024] = { "HTTP/1.1 200 OK\r\n\r\n" };
+  char badresponse[1024] = 
+    "HTTP/1.1 404 Not Found\r\n\r\n<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\r\n<TITLE>Not Found</TITLE></HEAD><BODY>\r\n<H1>Not Found</H1>\r\n</BODY></HTML>\r\n\r\n";
+  char goodresponse[1024] = "HTTP/1.1 200 OK\r\n\r\n";
   int fh;
   fh = OpenFile (buf, n);
 
@@ -92,16 +84,13 @@ HandleResonse (int fd, char *buf, int n)
     }
 }
 
-void *
-handleRequest (void *arg)
+void *handleRequest (void *arg)
 {
-
   struct thread *t = (thread *) arg;
   int n;
 
   for (;;)
     {
-
       while (!t->Running)
 	{
 	  usleep (2000);
@@ -116,13 +105,27 @@ handleRequest (void *arg)
 	
       close (t->fd);
       t->Running = 0;
+
+      pthread_mutex_lock(&worklist_mutex);
+      if (worklist == t)
+	worklist = t->next;
+      if (t->next)
+	t->next->prev = t->prev;
+      if (t->prev)
+	t->prev->next = t->next;
+      pthread_mutex_unlock(&worklist_mutex);
+
+      pthread_mutex_lock(&freelist_mutex);
+      t->next = freelist;
+      if (freelist)
+	freelist->prev = t;
+      freelist = t;
+      pthread_mutex_unlock(&freelist_mutex);
     }
 }
 
-void
-ThreadInitialize (int ThreadCount)
+void ThreadInitialize (int ThreadCount)
 {
-
   struct thread *t;
   int i;
 
@@ -130,88 +133,87 @@ ThreadInitialize (int ThreadCount)
 
   for (i = 0; i < ThreadCount; i++)
     {
-      t = (struct thread *) calloc (sizeof (thread), 1);
+      t = (struct thread *) calloc (1, sizeof(thread));
 
+      pthread_mutex_lock(&freelist_mutex);
       // add new thread structure to freelist 
       t->next = freelist;
-      t->prev = NULL;
       freelist = t;
+      pthread_mutex_unlock(&freelist_mutex);
 
-      if (pthread_create (&t->thread, NULL, &handleRequest, t) == -1)
+      if (pthread_create (&t->thread, NULL, &handleRequest, t) != 0)
 	{
 	  perror ("pthread create failed\n");
 	  exit (1);
 	}
     }
-
 }
 
-
-void
-ThreadCheck ()
+void ThreadCheck ()
 {
-  struct thread *t = freelist;
+  pthread_mutex_lock(&worklist_mutex);
+  struct thread *t = worklist;
+  while (t != NULL)
+    {
+      if (!t->Running)
+	{
+	  // remove thread structure from worklist 
+	  if (worklist == t)
+	    worklist = t->next;
+	  if (t->next)
+	    t->next->prev = t->prev;
+	  if (t->prev)
+	    t->prev->next = t->next;
 
-  // If a thread on the worklist is not running, move it to the head of the freelist;
-
-  if(!t->Running) {
-  
-  // remove thread structure from worklist 
-
-
-  if (worklist == t)
-    worklist = t->next;
-  if (t->next)
-    if (t->prev)
-      t->prev->next = t->next;
-    else 
-      t->next->prev = NULL;
-  if (t->prev)
-    if (t->next)
-      t->next->prev = t->prev;
-    else
-      t->prev->next = NULL;
-
-  // add thread structure to freelist 
-  t->next = freelist;
-  if (t->next)
-    t->next->prev = t;  
-  t->prev = NULL;
-  freelist = t;
-  }
-
+	  pthread_mutex_unlock(&worklist_mutex);
+	  pthread_mutex_lock(&freelist_mutex);
+	  // add thread structure to freelist 
+	  t->next = freelist;
+	  if (freelist)
+	    freelist->prev = t;
+	  freelist = t;
+	  pthread_mutex_unlock(&freelist_mutex);
+	  return;
+	}
+      t = t->next;
+    }
+  pthread_mutex_unlock(&worklist_mutex);
 }
 
-
-void
-ThreadHandleResponse (int fd)
+void ThreadHandleResponse (int fd)
 {
-
   struct thread *t;
 
   /* wait for any child process to free up */
-  while (freelist == NULL)
+  while (1)
     {
+      pthread_mutex_lock(&freelist_mutex);
+      if (freelist != NULL)
+	{
+	  // remove thread structure from freelist 
+	  t = freelist;
+	  freelist = t->next;
+	  if (freelist)
+	    freelist->prev = NULL;
+	  pthread_mutex_unlock(&freelist_mutex);
+
+	  pthread_mutex_lock(&worklist_mutex);
+	  // add thread structure to worklist 
+	  t->next = worklist;
+	  if (worklist)
+	    worklist->prev = t;
+	  worklist = t;
+	  pthread_mutex_unlock(&worklist_mutex);
+
+	  // setup thread and run it
+	  t->fd = fd;
+	  t->Running = 1;
+	  return;
+	}
+      pthread_mutex_unlock(&freelist_mutex);
+
       ThreadCheck ();
       sched_yield();
     }
-
-  // remove thread structure from freelist 
-  t = freelist;
-  freelist = t->next;
-  if (t->next)
-    t->next->prev = NULL;
-  t->prev = NULL;
-
-  // add thread structure to worklist 
-  t->next = worklist;
-  if (t->next)
-    t->next->prev = t;  
-  t->prev = NULL;
-  worklist = t;
-
-  // setup thread and run it
-  t->fd = fd;
-  t->Running = 1;
-
 }
+
